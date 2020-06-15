5a706e604d5d61813f14f467d98feac31ff2071c
gido5731 <celtictarot386@gmail.com>
2020-06-14 20:11:37 -0400

    add fcgi comment

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func selectRandom(s string) string {
 	}
 }
 func writeFile(w http.ResponseWriter, r *http.Request) {
-	fp := filepath.Join(selectRandom(r.URL.Path))
+	fp := r.URL.Path
 	if _, err := os.Stat(fp); err != nil {
 		fmt.Fprintf(w, "<!-- invalid image: %v -->\n", r.URL.Path)
 	}
@@ -120,5 +120,5 @@ func main() {
 		Addr:    fmt.Sprintf("%v:%v", *host, *port),
 		Handler: worker,
 	}
-	log.Fatal(h.ListenAndServe())
+	log.Fatal(h.ListenAndServe()) // this should be fcgi but I cant be arsed
 }
