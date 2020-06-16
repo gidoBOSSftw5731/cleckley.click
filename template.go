c770dbf9eb5d301f82e3b9dd055963f80ae34e45
gido5731 <celtictarot386@gmail.com>
2020-06-16 03:38:09 -0400

    pleasedontsuepleasedontsuepleasedontsue

--- template.go
+++ template.go
@@ -21,7 +21,7 @@ img {
     <img src="{{ .Main }}">
     </center>
 
-  <footer><mark>This site is not affiliated with Ms.Cleckley</mark></footer>
+  <footer><mark>This site is not affiliated with Ms.Cleckley, All data is of our users, and may not share the beliefs of the owners of this site or Ms.Cleckley. For contact, please email webmaster@cleckley.click</mark></footer>
   </body>
 </html>
 `
